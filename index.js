9d4675e375e420d5b305a45989a0895680bfe11f
ThiagoMR00781 <130001220+ThiagoMR00781@users.noreply.github.com>
2024-01-31 19:50:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { cwd } = require('process')
+const { cwd, constrainedMemory } = require('process')
 const { join } = require('path')
 const { error } = require('console')
-const { ifError } = require('assert')
+const { ifError, notStrictEqual } = require('assert')
 
 const app = express()
 
@@ -37,20 +37,48 @@ app.listen(5000, () => {
 })
 
 app.get('/pessoa', (req, res) => {
-    return res.json([DB])
+    return res.json(DB)
 })
 
 function buscaID(id){
     return DB.findIndex(DB => DB.id == id)
 }
+
 app.get('/pessoa/:id', (req, res) => {
     let id = buscaID(req.params.id)
-    res.json(DB[id])
+    if (id == -1) {
+        res.status(404).json({error: "ID não encontrado"})
+    } else {
+        res.status(201).json(DB[id])
+    }
 })
 
+function geradorID(length){
+    const buffer = crypto.randomBytes(length)
+    const numberId = buffer.toString('hex')
+    return parseInt(numberId, 16)
+}
+
+//let body = DB.push(req.body)
+ //   res.send('A pessoa foi cadastrada no DB').json(pessoa)
 app.post('/pessoa', (req, res) => {
-    DB.push(req.body)
-    res.status(201).send('A pessoa foi cadastrada no DB')
+   let {id, name} = req.body
+
+   let existepessoa = DB.find((pessoa) => pessoa.id == id)
+
+   if(existepessoa){
+    return res.status(400).json({ message: 'Pessoa já foi cadastrada'})
+   }
+   
+   if(!id){
+    const randomId = geradorID(8)
+    req.body.id = randomId
+   }
+
+   const novapessoa = { id: req.body.id, name: req.body.name}
+   DB.push(novapessoa)
+
+   res.status(201).json({ massage: 'Nova pessoa registrada com sucesso', pessoa: novapessoa})
 })
 
 app.put('/pessoa/:id', (req, res) => {
